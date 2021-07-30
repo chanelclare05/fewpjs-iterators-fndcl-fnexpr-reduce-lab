6d117ae10903f94a3240d63d6ed0f0ef802673fc
Chanel Donahoe <cdonahoe05@gmail.com>
2021-07-30 17:15:22 +1000

    done

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((accum, cv) => accum + cv);
+
+
+// Example code 
+// const products = [
+//     { name: 'Shampoo', price: 4.99 },
+//     { name: 'Donuts', price: 7.99 },
+//     { name: 'Cookies', price: 6.49 },
+//     { name: 'Bath Gel', price: 13.99 }
+//   ];
+
+//   function getTotalAmountForProducts(products) {
+//       let totalPrice = 0;
+//       for (const product of products) {
+//           totalPrice += product.price;
+//       };
+//       return totalPrice;
+//   }
+
+//   console.log(`This is the reduce method and it produced ${getTotalAmountForProducts(products)} as a result`);
+//   console.log(getTotalAmountForProducts(products));
+
+
+  
